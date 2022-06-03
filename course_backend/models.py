357a3ce6a40c7374f9907216ca1540f29408a562
Shaheen <coredeveloper.2013@gmail.com>
2022-06-03 21:39:49 +0600

    model fixed

--- course_backend/models.py
+++ course_backend/models.py
@@ -10,36 +10,36 @@ CUSTOMER = 3
 Base = declarative_base()
 
 
-class Users(Base):
+class BaseModel:
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(Date, default=date.today())
+    updated_at = Column(Date, default=date.today())
+
+
+class Users(Base, BaseModel):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     user_type = Column(Integer, nullable=False, default=1)
     address = Column(String, nullable=True)
-    created_at = Column(Date, default=date.today())
-    updated_at = Column(Date, default=date.today())
+    role = Column(String, nullable=True)
 
 
-class Courses(Base):
+class Courses(Base, BaseModel):
     __tablename__ = 'courses'
 
-    id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     title = Column(String)
     price = Column(Float)
     thumbnail_url = Column(String, nullable=True)
-    created_at = Column(Date, default=date.today())
-    updated_at = Column(Date, default=date.today())
 
 
-class Videos(Base):
+class Videos(Base, BaseModel):
     __tablename__ = 'videos'
 
-    id = Column(Integer, primary_key=True, index=True)
     course_id = Column(Integer, ForeignKey('courses.id'), nullable=False)
     title = Column(String)
     description = Column(String, nullable=True)
@@ -47,17 +47,12 @@ class Videos(Base):
     thumbnail_url = Column(String, nullable=True)
     duration = Column(Integer)
     free_preview = Column(Boolean, default=False)
-    created_at = Column(Date, default=date.today())
-    updated_at = Column(Date, default=date.today())
 
 
-class Purchase(Base):
+class Purchase(Base, BaseModel):
     __tablename__ = 'purchases'
 
-    id = Column(Integer, primary_key=True, index=True)
     course_id = Column(Integer, ForeignKey('courses.id'), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     purchase_amount = Column(Float)
-    created_at = Column(Date, default=date.today())
-    updated_at = Column(Date, default=date.today())
 
