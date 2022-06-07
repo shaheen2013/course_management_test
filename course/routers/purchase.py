52be50ad69a1aac255af89441a0eabd4fef00c79
Shaheen <coredeveloper.2013@gmail.com>
2022-06-07 18:41:07 +0600

    purchase list fixed

--- course/routers/purchase.py
+++ course/routers/purchase.py
@@ -4,9 +4,10 @@ from fastapi.responses import JSONResponse
 from course.config.database import Session
 from course.models.course import Purchase
 from course.models.users import Users, UserType
-from course.schemas import CreatePurchase, TokenUser
+from course.schemas import CreatePurchase, TokenUser, PageData
 from course.authentication import oauth2
 from course.utils import *
+from fastapi.encoders import jsonable_encoder
 
 
 router = APIRouter(
@@ -27,3 +28,17 @@ async def purchase_create(request: CreatePurchase, current_user: TokenUser = Dep
         session.commit()
         session.close()
         return JSONResponse(status_code=200, content=get_success_msg())
+
+
+@router.post("/list")
+async def get_purchases(page: PageData, current_user: TokenUser = Depends(oauth2.get_current_user)):
+    data = page.dict()
+    page = data.get("page")
+    page_size = data.get("page_size")
+    if (page_size > 100 or page_size < 0):
+        page_size = 100
+    page -= 1
+    session = Session()
+    purchases = session.query(Purchase).limit(page_size).offset(page * page_size).all()
+    session.close()
+    return JSONResponse(status_code=200, content=get_response_data(jsonable_encoder({"purchases": purchases})))
