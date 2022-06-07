feac136ec94d2505687a0b15406d3ed9f9405742
Shaheen <coredeveloper.2013@gmail.com>
2022-06-07 19:16:46 +0600

    env issue fixed

--- course/authentication/token.py
+++ course/authentication/token.py
@@ -1,10 +1,7 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from course import schemas
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from course.config.settings import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 
 def create_access_token(data: dict):
--- course/config/settings.py
+++ course/config/settings.py
@@ -5,8 +5,14 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-# DATABASE_URL = "postgresql://postgres:1234@0.0.0.0:5432/course"
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DOMAIN_NAME = "http://domain.com"
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+
+
